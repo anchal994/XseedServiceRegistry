cce185128e460e0c0d48bcf5da41a712df1f00e2
Roshan-Farkate <138759751+Roshan-Farkate@users.noreply.github.com>
2024-02-27 14:28:37 +0530

    Update XseedServiceRegistryApplicationTests.java

--- src/test/java/com/javatechie.java/XseedServiceRegistryApplicationTests.java
+++ src/test/java/com/javatechie.java/XseedServiceRegistryApplicationTests.java
@@ -3,7 +3,7 @@ package com.javatechie;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = XseedServiceRegistryApplication.class)
+@SpringBootTest(classes = com.xseedEureka.XseedServiceRegistryApplication.class)
 class XseedServiceRegistryApplicationTests {
 
 	@Test
